c2796e7b4ff4b04da19ec9b637dd66046983baa9
Amaury Daniel Palao Garcia <pala2402@usherbrooke.ca>
2026-06-06 19:09:17 -0400

    fix the display slowness

--- test.py
+++ test.py
@@ -1,76 +1,24 @@
-from timeWalk_functions import *
-from tools import *
+import pyqtgraph as pg
+import numpy as np
 
-if __name__ == "__main__":
+app = pg.mkQApp()
 
-    tot, tof, size = readBinaryWeerocFileWithPicoCalibrated(
-        "~/Documents/data/measures/21_avril/data_section_2_Btot49_Btoa12_Gain10_Thr700_56-65V_Freq80000_60s.bin"
-    )
+n = 1_000_000
 
-    nbrCanal = 8
-    canalFiltered = []
-    binWidth = 12
+x = np.random.rand(n)
+y = np.random.rand(n)
 
-    threshold = 2400
+w = pg.PlotWidget()
+w.show()
 
-    for i in range(size):
-        if tot[i] > threshold and tot[i] < 4E4 and tof[i] < 4.5E4:
-            canalFiltered.append((tot[i], tof[i]))
+scatter = pg.ScatterPlotItem(
+    x=x,
+    y=y,
+    size=2,
+    pen=None,
+    brush='w'
+)
 
-    rawCanals, rawToTCanals, rawToFCanals = split_canal_by_number(canalFiltered, nbrCanal)
-    correction_coefficients_timewalk_kde, correctedToFCanals_kde = timeWalkCorrection_kde(rawCanals)
-    correctedHistogramToF, correctedHistogramToF_canals = canals_to_histogram(correctedToFCanals_kde, binWidth=binWidth)
+w.addItem(scatter)
 
-    print(f"Correction coefficients (kde): {correction_coefficients_timewalk_kde}")
-
-    plt.close()
-    fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(8, 6))
-
-    for index in range(nbrCanal):
-        ax1.scatter(rawToTCanals[index],
-                    rawToFCanals[index],
-                    label=f"Canal {index}",
-                    s=5,
-                    alpha=0.4,
-                    edgecolors="none")
-        
-    ax1.set_xlabel("ToT")
-    ax1.set_ylabel("ToF")
-    ax1.set_title("Raw Data")
-    ax1.legend()
-
-    arr = np.array(rawToTCanals, dtype=object)
-
-    for index in range(nbrCanal):
-        ax2.scatter(rawToTCanals[index],
-                    correctedToFCanals_kde[index],
-                    label=f"Canal {index}",
-                    s=5,
-                    alpha=0.4,
-                    edgecolors="none")
-        
-    ax2.set_xlabel("ToT")
-    ax2.set_ylabel("ToF")
-    ax2.set_title("Corrected Raw Data")
-    ax2.legend()
-    
-    ax3.plot(
-        correctedHistogramToF[0],
-        correctedHistogramToF[1],
-        label="Corrected Histogram",
-    )
-
-    originalHistogramX, originalHistogramY = histogram([p[1] for p in canalFiltered], binWidth=binWidth)
-
-    ax3.plot(
-        originalHistogramX,
-        originalHistogramY,
-        label="Original Histogram",
-    )
-
-    ax3.set_xlabel("ToF")
-    ax3.set_ylabel("Count")
-    ax3.set_title("Combined Histogram")
-    ax3.legend()
-
-    plt.show()
+app.exec()
--- timeWalk_discret.py
+++ timeWalk_discret.py
@@ -1,169 +1,87 @@
 from Scripts_Radiopico.ReadBinaryWeeroc import *
+import pyqtgraph as pg
 import numpy as np
-import matplotlib.pyplot as plt
-from tools import *
 from scipy.stats import gaussian_kde
-from scipy.stats import exponnorm
 
-def round_to_nearest_step(values, step):
-    """
-    Round values to nearest multiple of step.
-    """
+def timeWalk_discrete(tof):
+    returnArray = {}
 
-    return np.round(values / step) * step
+    tof_sorted = np.sort(tof)
 
+    for index, event in enumerate(tof_sorted):
+        if event in returnArray:
+            returnArray[event] = returnArray[event] + 1
+        else:
+            returnArray[event] = index + 1
 
-def emg_curve(mu, sigma, tau, xmin=None, xmax=None, n=1000):
-    """
-    Generate an exponentially modified Gaussian (EMG) curve.
+    value = list(returnArray.keys())
+    count = list(returnArray.values())
 
-    Parameters
-    ----------
-    mu : float
-        Mean of the Gaussian component.
-    sigma : float
-        Standard deviation of the Gaussian component.
-    tau : float
-        Decay constant of the exponential component.
-    xmin, xmax : float, optional
-        Plot range. If None, chosen automatically.
-    n : int
-        Number of points.
-
-    Returns
-    -------
-    x, y : np.ndarray
-        Coordinates of the EMG PDF.
-    """
-
-    K = tau / sigma
-
-    if xmin is None:
-        xmin = mu - 5 * sigma
-
-    if xmax is None:
-        xmax = mu + 5 * sigma + 5 * tau
-
-    x = np.linspace(xmin, xmax, n)
-    y = exponnorm.pdf(x, K=K, loc=mu, scale=sigma)
-
-    return x, y
-
-def emg_samples(mu, sigma, tau, size=10000):
-    K = tau / sigma
-    return exponnorm.rvs(K=K, loc=mu, scale=sigma, size=size)
+    return value, count, returnArray
 
 if __name__ == "__main__":
-
-    
-    tot, data, size = readBinaryWeerocFileWithPicoCalibrated(
+    tot, tof, size = readBinaryWeerocFileWithPicoCalibrated(
         "~/Documents/data/measures/21_avril/data_section_2_Btot49_Btoa12_Gain10_Thr700_56-65V_Freq80000_60s.bin"
     )
     
-    '''
-    tofBin = 12
-    tofStep = 0.5
-    plt.scatter(
-        tof,
-        [p for p in range(len(tof))],
-        label = "Raw Data"
-    )
+    value, count, sigmoid = timeWalk_discrete(tof)
+    normale_count = count / np.max(count)
 
-    sorted_tof = np.sort(tof)
-    discrete_counts = { rounded_s: 0 for rounded_s in np.arange(round_to_nearest_step(sorted_tof[0], tofStep), round_to_nearest_step(sorted_tof[-1], tofStep) + 1, tofStep) }
+    derivate_sigmoid = np.gradient(count, value)
+    normale_derivate_sigmoid = derivate_sigmoid / np.max(derivate_sigmoid)
 
-    for s in sorted_tof:
-        rounded_s = float(round_to_nearest_step(s, tofStep))
+    kde = gaussian_kde(tof)
 
-        discrete_counts[rounded_s] += 1
-    
-    plt.scatter(
-        list(discrete_counts.keys()),
-        list(discrete_counts.values()),
-        label = "Cumulative Counts",
-        color = "red"
-    )
-    '''
-
-    tofStep = 0.5
-    #data = emg_samples(mu=1000, sigma=200, tau=300, size=10000)
-
-    kde = gaussian_kde(data)
-
-    x_kde = np.linspace(min(data), max(data), 1000)
+    x_kde = np.linspace(min(tof), max(tof), 10000)
     y_kde =  kde(x_kde)
     y_kde = (y_kde / np.max(y_kde))
 
-    '''
-    plt.scatter(
-        data,
-        [p for p in range(len(data))],
-        label = "Raw Data",
-        alpha = 0.5,
-        s = 5,
-        edgecolors = "none"
-    )
-    '''
+    # -------------------------
 
-    plt.scatter(
-        x_kde,
-        y_kde,
-        label = "KDE",
-        color = "red",
-        alpha = 0.5,
-        s = 5,
-        edgecolors = "none"
-    )
+    app = pg.mkQApp()
 
-    sorted_tof = np.sort(data)
-    discrete_counts = { rounded_s: 0 for rounded_s in np.arange(round_to_nearest_step(sorted_tof[0], tofStep), round_to_nearest_step(sorted_tof[-1], tofStep) + 1, tofStep) }
+    plt = pg.PlotWidget()
+    plt.show()
+    plt.showGrid(x = True, y = True)
+    plt.addLegend()
 
-    for s in sorted_tof:
-        rounded_s = float(round_to_nearest_step(s, tofStep))
+    # set properties of the label for y axis
+    plt.setLabel('left', 'Vertical Values', units ='y')
 
-        discrete_counts[rounded_s] += 1
+    # set properties of the label for x axis
+    plt.setLabel('bottom', 'ToF', units ='ps')
 
-    cumulative_counts = np.cumsum(list(discrete_counts.values()))
-
-    plt.scatter(
-        list(discrete_counts.keys()),
-        cumulative_counts/np.max(cumulative_counts),
-        label = "Normalized Cumulative Counts (Cumsum)",
-        alpha = 0.5,
-        s = 5,
-        edgecolors = "none"
+    scatter_sigmoid = pg.ScatterPlotItem(
+        x=value,
+        y=normale_count,
+        size=2,
+        pen='g',
+        brush='w',
+        name="sigmoid"
     )
 
-    derivative = np.diff(y_kde) / np.diff(x_kde)
-    normale_derivative = derivative/np.max(derivative)
-
-    plt.scatter(
-        x_kde[:-1],
-        normale_derivative,
-        label = "Normalized Derivative of Cumulative Counts",
+    scatter_kde = pg.ScatterPlotItem(
+        x=x_kde,
+        y=y_kde,
+        size=2,
+        pen='r',
+        brush='w',
+        name="derived sigmoid"
     )
 
-    threshold = -0.008
+    scatter_derivate_sigmoid = pg.ScatterPlotItem(
+        x=value,
+        y=normale_derivate_sigmoid,
+        size=2,
+        pen='m',
+        brush='w',
+        name="derived sigmoid"
+    )
 
-    peak_index = None
+    plt.addItem(scatter_sigmoid)
+    plt.addItem(scatter_kde)
+    plt.addItem(scatter_derivate_sigmoid)
+    plt.addLine(x=x_kde[np.argmax(y_kde)], pen=pg.mkPen('r', width=3))
 
-    for index, value in enumerate(normale_derivative[:100]):
-        
-        if value < threshold:
-            peak_index = index
-            break
+    app.exec()
 
-
-    peak_estimated_x = x_kde[peak_index]
-
-    print("Estimated Peak ToF:", peak_estimated_x)
-    print("KDE Peak ToF:", x_kde[np.argmax(y_kde)])
-    print(f"Difference between estimated peak and true peak: {abs(peak_estimated_x - x_kde[np.argmax(y_kde)])}")
-
-
-    plt.xlim(0, 100000)
-    plt.ylabel("Y")
-    plt.xlabel("ToF")
-    plt.legend()
-    plt.grid(True)
-    plt.savefig("peak_finder.png")
